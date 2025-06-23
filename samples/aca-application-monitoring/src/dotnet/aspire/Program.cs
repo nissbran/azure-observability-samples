65867d327ca905301886f86cffdabc288750d2fa
Kristofer Nissbrandt <nissbran@gmail.com>
2025-06-23 14:52:15 +0200

    Update packages

--- samples/aca-application-monitoring/src/dotnet/aspire/Program.cs
+++ samples/aca-application-monitoring/src/dotnet/aspire/Program.cs
@@ -5,34 +5,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.local.json", true);
 
 var sql = builder.AddAzureSqlServer("sql")
-    .RunAsContainer(resourceBuilder => resourceBuilder.WithLifetime(ContainerLifetime.Persistent));
+    .RunAsContainer(container =>
+    {
+        container.WithLifetime(ContainerLifetime.Persistent);
+    });
 
 var creditDb = sql.AddDatabase("credit-db");
 var bookingDb = sql.AddDatabase("booking-db");
 
-IResourceBuilder<IResourceWithConnectionString> serviceBus;
-
-if (builder.ExecutionContext.IsPublishMode)
-{
-    serviceBus = builder.AddAzureServiceBus("messaging");
-}
-else
-{
-    var sqlPasswordParameter = sql.Resource.ConnectionStringExpression.ValueProviders
-        .FirstOrDefault(provider => provider is ParameterResource { Name: "sql-password" }) as ParameterResource;
-
-    var sbEmulatorConfigAbsolutePath = Path.GetFullPath("ServiceBusConfig.json");
-
-    builder.AddContainer("servicebusemulator", "mcr.microsoft.com/azure-messaging/servicebus-emulator", "latest")
-        .WithBindMount(sbEmulatorConfigAbsolutePath, "/ServiceBus_Emulator/ConfigFiles/Config.json")
-        .WithEnvironment("SQL_SERVER", "sql")
-        .WithEnvironment("MSSQL_SA_PASSWORD", sqlPasswordParameter?.Value)
-        .WithEnvironment("ACCEPT_EULA", "Y")
-        .WithHttpEndpoint(5672, 5672)
-        .WaitFor(sql);
-    
-    serviceBus = builder.AddConnectionString("messaging");
-}
+var serviceBus = builder.AddAzureServiceBus("messaging")
+    .RunAsEmulator(emulator =>
+    {
+        emulator.WithConfigurationFile(
+            path: "./ServiceBusConfig.json");
+    });
 
 var dbSetup = builder.AddProject<Projects.DbSetup>("db-setup")
     .WithReference(creditDb)
@@ -47,7 +33,8 @@ builder.AddProject<Projects.CreditApi>("credit-api")
 builder.AddProject<Projects.BookingProcessor>("booking-processor")
     .WithReference(serviceBus)
     .WithReference(bookingDb)
-    .WaitForCompletion(dbSetup);
+    .WaitForCompletion(dbSetup)
+    .WaitFor(serviceBus);
 
 builder.AddProject<Projects.BookingApi>("booking-api")
     .WithReference(bookingDb)
